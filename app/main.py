6e76d7379e181cf7d0ca9578b22abd9693d9837d
Yeseviola <aryesigon@gmail.com>
2025-05-02 22:47:13 -0300

    actualizacion de algunas cosas como proxi cors

--- app/main.py
+++ app/main.py
@@ -28,7 +28,7 @@ def create_app() -> FastAPI:
     #CORS abierto durante desarrollo para descartar bloqueos
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:5173"],     
+        allow_origins=["http://localhost:5173","http://127.0.0.1:5173"],     
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["Authorization",
